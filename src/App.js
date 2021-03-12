6cf84960ff11f01ed4caac2c109a59e9906414ff
Sam Jo <jo.sunghak@gmail.com>
2021-03-12 17:45:52 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import SearchPage from './components/SearchPage';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<div className="app">
+      <Router>
+        <Switch>
+          <Route path='/search'>
+            <SearchPage />
+          </Route>
+          <Route path='/'>
+            <Home/>
+          </Route>
+        </Switch>
+      </Router>
+
+      
+              {/* Home (the one with the search on) */}
+              {/* SearchPage (Results Page) */}
     </div>
   );
 }
---
+++ src/keys.js
@@ -0,0 +1,4 @@
+//GET https://www.googleapis.com/customsearch/v1?key=AIzaSyAoXN53koxAlGKKPFNTRmIKiloIgMMFNts&cx=017576662512468239146:omuauf_lfve&q=lectures
+
+export const API_KEY = 'AIzaSyAoXN53koxAlGKKPFNTRmIKiloIgMMFNts'
+export default API_KEY;
